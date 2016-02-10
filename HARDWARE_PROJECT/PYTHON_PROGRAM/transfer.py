282d17610b1b56787b8ca9c3ee44ef12ba07f440
Matteo Danieletto <mdanieletto@eng.ucsd.edu>
2016-02-09 17:03:44 -0800

    added bluetooth and fixed some bugs

--- HARDWARE_PROJECT/PYTHON_PROGRAM/transfer.py
+++ HARDWARE_PROJECT/PYTHON_PROGRAM/transfer.py
@@ -5,17 +5,22 @@ import csv
 import string
 import hashlib
 
+
+#BLUETOOTH LIBRARY
+import bluetooth
+#MATTEO'LIBRARY
 import readFiles
 
 def temperature(stepResolution, stepExtTemp, stepIntTemp, V20C):
-	
+	print stepIntTemp	
 	extTemp = ((stepResolution * int(stepExtTemp))-500)/10;
 	intTemp = (((stepResolution * int(stepIntTemp)* 0.001)-V20C)/0.001)-20;
 
 	temperatureValues= [extTemp, intTemp];
 	label= ['External','Internal'];
-	print(temperatureValues)
-
+	print(label);
+	print(temperatureValues);
+	return temperatureValues
 
 
 def volt2PPB(fileValues,stepResolution, stepWE, stepAE, pollution, temperature):
@@ -29,10 +34,11 @@ def volt2PPB(fileValues,stepResolution, stepWE, stepAE, pollution, temperature):
 			zeroVoltageWE 	= int(row[1]);
 			zeroVoltageAE	= int(row[2]);
 			ppbOvermV	= float(row[3]);
-	for row in fileValues.readTemperatureTable()
-		if(row[0]==pollution)
-			n		= row[(int(temperature)/10)+1];
-	
+	for row in fileValues.readTemperatureTable():
+		if(row[0]==pollution):
+			index = (int(round(temperature)+30)/10)+1
+			n = float(row[int(index)])
+			print n	
 
 
 	voltageWE 	= stepResolution * stepWE;
@@ -74,14 +80,16 @@ def mainLoop(fileValuesa, ser, csv_file):
 				count = count -1;
 				temp = temperature(stepResolution, listValue[-1], listValue[-2], V20C)	;
 				
-				CO_ppb=volt2Pollution(fileValues, stepResolution, float(listValue[1]), float(listValue[2]),'COA4', temp);
-				O3_ppb=volt2Pollution(fileValues, stepResolution, float(listValue[3]), float(listValue[4]), 'O3A4', temp);
+				CO_ppb=volt2PPB(fileValues, stepResolution, float(listValue[1]), float(listValue[2]),'COA4', temp[1]);
+				O3_ppb=volt2PPB(fileValues, stepResolution, float(listValue[3]), float(listValue[4]), 'O3A4', temp[1]);
 				print (arrayLabel);
 				print (listValue);
 				print("CO ppb:" + str(CO_ppb));			
 				print("O3 ppb:" + str(O3_ppb));			
 				if(count == 0):
 					print("SEND DATA: " + str(listValue));
+					string2Send =ts +','+str(CO_ppb) +','+str(O3_ppb)
+					sock.send(string2Send)
 					count  = timesample2SendPacket;
 			
 			#except ser.SerialTimeoutException:
@@ -97,6 +105,14 @@ if __name__ == "__main__":
 	zeroOffsetFile ='ZERO_A4_25000014.csv';
 	fileValues = readFiles.readFiles(pathconfig , temperatureFile,zeroOffsetFile)
 
+
+	bd_addr = "5C:31:3E:DF:AC:9E"
+	port = 1
+	sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	sock.connect((bd_addr,port))
+
+
+
 	ser = serial.Serial('/dev/ttyMCC', 115200, serial.EIGHTBITS , serial.PARITY_NONE ,timeout=1)
 
         randomname      = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
@@ -105,4 +121,4 @@ if __name__ == "__main__":
         csv_file        = open(dirName+filename, 'w')
 
 	
-	mainLoop(fileValues, ser, csv_file) 
+	mainLoop(fileValues, ser, csv_file, sock) 
