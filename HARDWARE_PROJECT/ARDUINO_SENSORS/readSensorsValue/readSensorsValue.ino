e5bd7392aa6c15ddc5672d49ff5f570c3f1244ea
Matteo Danieletto <mdanieletto@eng.ucsd.edu>
2016-02-09 17:51:40 -0800

    added bluetooth and fixed some bugs

--- HARDWARE_PROJECT/ARDUINO_SENSORS/readSensorsValue/readSensorsValue.ino
+++ HARDWARE_PROJECT/ARDUINO_SENSORS/readSensorsValue/readSensorsValue.ino
@@ -1,27 +1,24 @@
 #include <digitLibrary.h>
-
-
 #define LOOP_DEFAULT_VALUE 1000
-int pinLed = 0;
+int pinLed = 2;
 
 //Analog Input from the Sensor board
-int PT_p  = A0;
+//PM2.5
+int PM2_5 = A0;
+
 
 int CO_WE = A1;
 int CO_AE = A2;
 
 int O3_WE = A3;
 int O3_AE = A4;
-//PM2.5
-int PM2_5 = A5;
+int PT_p  = A5;
 
 //PIN 7 way
 int pinDigit      = 11;
 int pinDigit2     = 12;
 int pinDigit3     = 13;
 int pinDigit4     = 14;
-
-
 
 int arraySegments[7];
 int arrayDigits[4];
@@ -31,6 +28,10 @@ int startPortSegment  = 4;
 int startPortDigit    = 11;
 
 
+int digit=8;
+int segment=9;
+
+
 int ctrl ;
 int delayLoop;
 void setup()
@@ -38,8 +39,14 @@ void setup()
   Serial.begin(115200);
   ctrl = 1;
   pinMode(pinLed, OUTPUT);  
-  delayLoop = LOOP_DEFAULT_VALUE;
 
+  pinMode(digit, OUTPUT);  
+  pinMode(segment, OUTPUT);  
+
+  digitalWrite(digit, HIGH);
+  digitalWrite(segment, HIGH);
+  delayLoop = LOOP_DEFAULT_VALUE;
+  analogReadResolution(12);
 
   int ii;
   for(ii = 0 ; ii < 7 ; ii++)
@@ -52,14 +59,16 @@ void setup()
     arrayDigits[ii] = ii + startPortDigit; 
   }
 
-  SegmentDevice controlLED(arraySegments,arrayDigits);
+//  SegmentDevice controlLED(arraySegments,arrayDigits);
+
+  digitalWrite(pinLed, HIGH);
 }
 
 
 
 void loop()
 {
-  analogReadResolution(12);
+ 
 
   Serial.print(analogRead(CO_WE));
   Serial.print(",");
@@ -78,35 +87,25 @@ void loop()
 
   if (ctrl == 0)
   {
-    //    digitalWrite(pinDigit, HIGH);
-    //    digitalWrite(pinDigit2, LOW);
-    //
-    //    digitalWrite(pinSegmentE, HIGH);
-    //
     digitalWrite(pinLed, HIGH);
-    //
     ctrl = 1;
   }
   else
   {
-    //    digitalWrite(pinDigit, LOW);
-    //    digitalWrite(pinDigit2, LOW);
-    //    digitalWrite(pinSegmentE, HIGH);
-    //
+   
     digitalWrite(pinLed, LOW);
-
     ctrl = 0;
   }
 
-  int tmpValue = Serial.read();
-  if (tmpValue != -1)
-  {
-    delayLoop = tmpValue;
-  }
-  else
-  {
-    delayLoop = LOOP_DEFAULT_VALUE;
-  }
+//  int tmpValue = Serial.read();
+//  if (tmpValue != -1)
+//  {
+//    delayLoop = tmpValue;
+//  }
+//  else
+//  {
+//    delayLoop = LOOP_DEFAULT_VALUE;
+//  }
   delay(delayLoop);
 }
 
