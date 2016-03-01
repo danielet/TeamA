00b0f9edcd84a37f580a8f3e14741582d6ac852f
matteo <matteo.danieletto@gmail.com>
2016-03-01 13:10:52 +0900

    new arduino sketch

--- HARDWARE_PROJECT/ARDUINO_SENSORS/readSensorsValue/readSensorsValue.ino
+++ HARDWARE_PROJECT/ARDUINO_SENSORS/readSensorsValue/readSensorsValue.ino
@@ -1,18 +1,25 @@
 #include <digitLibrary.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+Adafruit_ADS1015 ads1015;
+
 #define LOOP_DEFAULT_VALUE 1000
 int pinLed = 2;
 
 //Analog Input from the Sensor board
 //PM2.5
-int PM2_5 = A0;
+int NO2_WE = A0;
+int NO2_AE = A5;
 
+int CO_WE = A2;
+int CO_AE = A3;
 
-int CO_WE = A1;
-int CO_AE = A2;
-
-int O3_WE = A3;
+int O3_WE = A1;
 int O3_AE = A4;
-int PT_p  = A5;
+
+
+int LED = 2;
+
 
 //PIN 7 way
 int pinDigit      = 11;
@@ -37,77 +44,88 @@ int delayLoop;
 void setup()
 {
   Serial.begin(115200);
+   while (!Serial);
+  delay(100);
   ctrl = 1;
-  pinMode(pinLed, OUTPUT);  
-
-  pinMode(digit, OUTPUT);  
-  pinMode(segment, OUTPUT);  
-
-  digitalWrite(digit, HIGH);
-  digitalWrite(segment, HIGH);
-  
-  delayLoop = LOOP_DEFAULT_VALUE;
   analogReadResolution(12);
+  pinMode(LED, OUTPUT);  
 
-  int ii;
-  for(ii = 0 ; ii < 7 ; ii++)
-  {
-    arraySegments[ii] = ii + startPortSegment; 
-  }
 
-  for(ii = 0 ; ii < 4 ; ii++)
-  {
-    arrayDigits[ii] = ii + startPortDigit; 
-  }
-
-//  SegmentDevice controlLED(arraySegments,arrayDigits);
 
   digitalWrite(pinLed, HIGH);
+//I2C
+  ads1015.begin();
+  ads1015.setGain(GAIN_ONE);
+//  pinMode(LED, OUTPUT);  
 }
 
-
-
+int delayADC = 10;
+int count = 0;
 void loop()
 {
- 
-
-  Serial.print(analogRead(CO_WE));
-  Serial.print(",");
-  Serial.print(analogRead(CO_AE));
-  Serial.print(",");
-  Serial.print(analogRead(O3_WE));
-  Serial.print(",");
-  Serial.print(analogRead(O3_AE));
-  Serial.print(",");
-  Serial.print(analogRead(PT_p));
-  Serial.print(",");
-  Serial.print(analogRead(PM2_5));
-  Serial.println();
 
 
 
-  if (ctrl == 0)
-  {
-    digitalWrite(pinLed, HIGH);
-    ctrl = 1;
-  }
-  else
-  {
-   
-    digitalWrite(pinLed, LOW);
-    ctrl = 0;
-  }
+      if (Serial.available() > 0 && count == 1) {
+        
+        while(Serial.available() > 0){
+          Serial.read();
+        }
+          
+        
+        
+        int16_t temp = ads1015.readADC_SingleEnded(0);
+        int16_t pm25 = ads1015.readADC_SingleEnded(1);
 
-//  int tmpValue = Serial.read();
-//  if (tmpValue != -1)
-//  {
-//    delayLoop = tmpValue;
-//  }
-//  else
-//  {
-//    delayLoop = LOOP_DEFAULT_VALUE;
-//  }
-  delay(delayLoop);
+        int16_t CO_w = analogRead(CO_WE);
+        delay(delayADC);
+        int16_t CO_a = analogRead(CO_AE);
+        delay(delayADC);
+        int16_t O3_w = analogRead(O3_WE);
+        delay(delayADC);
+        int16_t O3_a = analogRead(O3_AE);
+        delay(delayADC);
+        int16_t NO2_w = analogRead(NO2_WE);
+        delay(delayADC);
+        int16_t NO2_a = analogRead(NO2_AE);
+        String ss = String(CO_w)+ ','+String(CO_a)+ ','+String(O3_w)+ ','+String(O3_a)+ ',' +String(NO2_w)+ ','+String(NO2_a)+ ',' + String(pm25)+ ','+String(temp);
+//        String ss = String(CO_w)+ ','+String(CO_a)+ ','+String(O3_w)+ ','+String(O3_a)+ ',' +String(NO2_w)+ ','+String(NO2_a)+ ',' + String(temp)+ ','+String(pm25)+',' +String(Serial.available());
+
+        Serial.println(ss);
+        
+        
+        digitalWrite(LED, HIGH);
+        delay(100);
+        digitalWrite(LED, LOW);
+
+        
+
+      }
+      if(count == 0){
+        count++ ;
+      }
 }
 
+
+
+//  pinMode(digit, OUTPUT);  
+//  pinMode(segment, OUTPUT);  
+//
+//  digitalWrite(digit, HIGH);
+//  digitalWrite(segment, HIGH);
+//  delayLoop = LOOP_DEFAULT_VALUE;
+
+//
+//  int ii;
+//  for(ii = 0 ; ii < 7 ; ii++)
+//  {
+//    arraySegments[ii] = ii + startPortSegment; 
+//  }
+//
+//  for(ii = 0 ; ii < 4 ; ii++)
+//  {
+//    arrayDigits[ii] = ii + startPortDigit; 
+//  }
+
+//  SegmentDevice controlLED(arraySegments,arrayDigits);
 
